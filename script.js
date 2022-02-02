7cc259ef2319dce9a1dbd8908b46577ec858a0e5
Ovidiu Goanta <Goantza_jr@yahoo.com>
2022-02-02 10:03:03 +0100

    password-check with Vue.js

---
+++ script.js
@@ -0,0 +1,38 @@
+Vue.createApp({
+  data() {
+    return {
+      passwordType: "password",
+      button: "Show Password",
+      passwordOne: "",
+      passwordTwo: "",
+    };
+  },
+  methods: {
+    changeType() {
+      if (this.passwordType === "password") {
+        this.passwordType = "text";
+        this.button = "Hide Password";
+      } else {
+        this.passwordType = "password";
+        this.button = "Show Password";
+      }
+    },
+  },
+  computed: {
+    containNumber() {
+      return /[0-9]/.test(this.passwordOne);
+    },
+    containUppercase() {
+      return /[A-Z]/.test(this.passwordOne);
+    },
+    containLowercase() {
+      return /[a-z]/.test(this.passwordOne);
+    },
+    samePassword() {
+      return this.passwordOne === this.passwordTwo && this.passwordOne !== "";
+    },
+    alLeast10Char() {
+      return this.passwordOne.length > 10;
+    },
+  },
+}).mount("#app");
